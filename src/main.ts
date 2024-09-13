a0cc759e1cd278b0e4e47600914c33157678bb90
Rashy-hub <yagoubi.r@gmail.com>
2024-09-13 23:42:46 +0200

    feat(main): Integrate factory pattern use case.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log('main.js entry point');
+console.log('main.ts entry point');
+
+// src/main.ts
+import { ProductFactory } from './Creational Patterns/FactoryPattern';
+
+function main() {
+  const physicalProduct = ProductFactory.createProduct('physical');
+  physicalProduct.display();
+
+  const digitalProduct = ProductFactory.createProduct('digital');
+  digitalProduct.display();
+
+  const serviceProduct = ProductFactory.createProduct('service');
+  serviceProduct.display();
+}
+
+main();
